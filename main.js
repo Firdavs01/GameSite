947385714d41ad5b7cf072ceb62d313c7ba9068a
Firdavs <firdavsstandoff@gmail.com>
2024-12-02 04:27:48 +0500

    end of verstka

--- main.js
+++ main.js
@@ -12,3 +12,26 @@ emailBtn.onclick = () => {
         alert('You passed!!!!');
     }
 }       
+
+// document.querySelector('#btnFr').addEventListener('click', () => {
+//     document.querySelector('.modal').classList.add('open');
+// })
+
+// document.querySelector('.close__btn').addEventListener('click', () => {
+//     document.querySelector('.modal').classList.remove('open');
+// })
+
+// window.addEventListener('keydown', () => {
+//     if (e.key === 'Escape') {
+//         document.querySelector(.modal).classList.remove('open');
+//     }
+// })
+
+// document.querySelector("#my-modal .modal__box").addEventListener('click', (event) => {
+//     event._isClickWithInModal = true;
+// })
+
+// document.getElementById('my-modal').addEventListener('click', (event) => {
+//     if (event._isClickWithInModal) return ;
+//     event.currentTarget.classList.remove('open');
+// })
